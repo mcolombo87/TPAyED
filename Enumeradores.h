081decc6667e2b9dfe8c392a6fed4145221c4889
Maxi <mcolombo87@gmail.com>
2015-06-18 20:54:35 -0300

    Enumeradores agregados

---
+++ Enumeradores.h
@@ -0,0 +1,9 @@
+#ifndef ENUMERADORES_H_INCLUDED
+#define ENUMERADORES_H_INCLUDED
+
+enum provincias {BuenosAires = 1, Catamarca, CiudadAutonomaBA, Chaco, Chubut, Cordoba, Corrientes, EntreRios, Formosa, Jujuy, LaPampa, LaRioja, Mendoza, Misiones, Neuquen, RioNegro,
+    Salta, SanJuan, SanLuis, SantaCruz, SantaFe, SantiagoDelEstero, TierraDelFuego, Tucuman} BA, CT, CABA, CC, CH, CBA, CR, ER, FO, JY, LP, LR, MZ, MN, NQ, RN, SA,
+    SJ, SL, SC, SF, SE, TF, TM;
+
+
+#endif // ENUMERADORES_H_INCLUDED
